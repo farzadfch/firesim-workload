656b686a4e08fe0d42e2edbfa7883e387a2444af
Farzad Farshchi <farzadfr@gmail.com>
2019-08-14 00:29:39 +0000

    add -i option for profile interval[4~

--- overlay/root/vision/run-profile.sh
+++ overlay/root/vision/run-profile.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-../riscv-hpmcounters/hpm_counters $HPM_COUNTER_INTERVAL > profile.csv &
+../riscv-hpmcounters/hpm_counters -i $HPM_INTERVAL > profile.csv &
 ${1} ${2}
 killall hpm_counters
 while pgrep hpm_counters > /dev/null; do usleep 1000; done
