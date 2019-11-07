a67cb3e65f38e073503358e5202c6bc0b14066d1
Farzad Farshchi <farzadfr@gmail.com>
2019-11-07 18:33:41 +0000

    ..

--- overlay/root/scripts/bw-loop.sh
+++ overlay/root/scripts/bw-loop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -o xtrace
 
-bandwidth-rt -s -c3 -t0 -awrite -m4096 -i1 -j1000 -l4100 > ${RES} 2> /dev/null
+bandwidth-rt -c3 -t0 -r99 -s -awrite -m512 -d1 -i2 -j1000 -l4100 > ${RES} 2> /dev/null
