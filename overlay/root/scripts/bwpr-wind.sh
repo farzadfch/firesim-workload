2b8af04ea2717e1be64ce59dde9016475c972099
Farzad Farshchi <farzadfr@gmail.com>
2020-02-14 01:17:14 +0000

    revert

--- overlay/root/scripts/bwpr-wind.sh
+++ overlay/root/scripts/bwpr-wind.sh
@@ -4,9 +4,9 @@ set -o xtrace
 . /root/env.sh
 RES=/root/results
 
-# 2ms
-set-c0123-quadcore-rdwr.sh 4259999 40000 4260000
-taskset 8 bandwidth-rt -m64 -i1 -j25 -l200 > $RES/bwpr-2ms.txt
+# 1ms
+set-c0-singlecore-rdwr.sh 2129999 20000 2130000
+bandwidth-rt -m64 -i1 -j25 -l200 > $RES/bwpr-1ms.txt
 # 200ns
-set-c0123-quadcore-rdwr.sh 425 4 426
-taskset 8 bandwidth-rt -m64 -i1 -j25 -l200 > $RES/bwpr-200ns.txt
+set-c0-singlecore-rdwr.sh 425 4 426
+bandwidth-rt -m64 -i1 -j25 -l200 > $RES/bwpr-200ns.txt
